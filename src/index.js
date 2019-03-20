1059c78acddfe35b66fdd5f57f7c71a6b4a1d6a4
Qairkhan <shyngys94@gmail.com>
2019-03-20 22:54:58 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let result = 0;
+  const arr = [];
+  for(let i=0; i < preferences.length; i++)
+  {
+    const personX = preferences[i];
+    const personY = preferences[personX - 1];
+    const personZ = preferences[personY - 1];
+    if (personZ - 1 === i && !arr.includes(personX) && personX !=personZ) {
+      result++;
+      arr.push(personX, personY, personZ);
+    }
+  }
+  return result;
 };
